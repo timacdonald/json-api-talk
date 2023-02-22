136ac44160498cc72e6e759b3e011e2e720f3dea
Tim MacDonald <hello@timacdonald.me>
2023-02-22 19:50:59 +1100

    wip

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,10 @@ use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * @var array<int, string>
